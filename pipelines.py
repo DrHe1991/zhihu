7694e7e6c4b44f73c96796fca9d83b4f347fc226
Dr. He <Drhe@localhost.localdomain>
2018-12-10 12:29:08 +0800

    zhihu initial commit

---
+++ pipelines.py
@@ -0,0 +1,60 @@
+from scrapy.exceptions import DropItem
+import pymongo
+class TextPipeline(object):
+    def __init__(self):
+        self.limit = 50
+    
+    def process_item(self,item,spider):
+    # item['educations'][0]['major']['introduction']
+    # item['educations'][0]['major']['excerpt']
+
+    # item['locations'][0]['introduction']
+    # item['locations'][0]['excerpt']
+
+        if item['educations']:
+            for education in item['educations']:
+                education['school']['introduction']=self.cut(education['school']['introduction'])
+                education['major']['introduction']=self.cut(education['major']['introduction'])
+                education['major']['excerpt']=self.cut(education['major']['excerpt'])
+        if item['locations']:
+            for location in item['locations']:
+                location['introduction']=self.cut(location['introduction'])
+                location['excerpt']=self.cut(location['excerpt'])
+        if 'business' in item.keys():
+            if item['business']:
+                item['business']['introduction']=self.cut(item['business']['introduction'])
+                item['business']['excerpt']=self.cut(item['business']['excerpt'])
+
+        return item
+
+
+
+    def cut(self, text):
+        if len(text) > self.limit:
+            text = text[0:self.limit].rstrip()+'...'
+        return text
+
+
+class MongoPipeline(object):
+    collection_name = 'users'
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+    
+    def process_item(self, item, spider):
+        self.db[self.collection_name].update({'url_token': item['url_token']}, {'$set': dict(item)}, True)
+        return item
+    
+    def close_spider(self, spider):
+        self.client.close()
---
+++ spiders/zhihu_user.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from scrapy import Spider, Request
+from zhihu.items import UserItem
+import time
+
+class ZhihuUserSpider(scrapy.Spider):
+    name = 'zhihu_user'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+    offset = 0
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    followers_url = 'https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&amp;offset={offset}&amp;limit={limit}'
+    start_user = 'yun-tian-38'
+    user_query = 'locations,employments,gender,educations,business,voteup_count,thanked_count,follower_count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,following_columns_count,answer_count,articles_count,pins_count,question_count,commercial_question_count,favorite_count,favorited_count,logs_count,marked_answers_count,marked_answers_text,message_thread_token,account_status,is_active,is_force_renamed,is_bind_sina,sina_weibo_url,sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage,badge[?(type=best_answerer)].topics'
+    followers_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+    def start_requests(self):
+        yield Request(self.user_url.format(user=self.start_user, include=self.user_query), self.parse_user)
+        yield Request(self.followers_url.format(user=self.start_user, include=self.followers_query, limit=20, offset=self.offset), self.parse_followers)
+    
+    # this is used to parse every user's info
+    def parse_user(self, response):
+        result = json.loads(response.text)
+        item = UserItem()
+        for field in item.fields:
+            if field in result.keys():
+                item[field] = result.get(field)
+        yield item
+
+    # this is used to parse followers' list
+    def parse_followers(self, response):
+        results = json.loads(response.text)
+        # loop the userlist and do the user info parse again 
+        if 'data' in results.keys():
+            for result in results.get('data'):
+                yield Request(self.user_url.format(user=result.get('url_token'), include=self.user_query),self.parse_user)
+                time.sleep(1)
+        # move to next page and do the follower's list parse again 
+        # if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
+        #     # next_page = results.get('paging').get('next')
+        #     # above is not working cuz next_page is not showing the right url
+        #     next_page = self.followers_url.format(user=self.start_user, include=self.followers_query, limit=20, offset=self.offset+20)        
+        #     yield Request(next_page,self.parse_followers)
+        #     self.offset += 20
+
